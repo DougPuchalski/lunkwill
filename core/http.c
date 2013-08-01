952a29184d37d224b8eb354f578aeb08ec798143
Florian Wernli <0pq76r@gmail.com>
2013-08-01 05:52:13 +0000

    Using stat for filesize

--- core/http.c
+++ core/http.c
@@ -23,13 +23,11 @@ int send_file(char **buffer, char *file_path){
 		return -1;
 	}
 	
-	fseek(file, 0, SEEK_END);
-	file_size = ftell(file);
-	rewind(file);
+	file_size = statbuf.st_size;
 
-	*buffer=(char *)malloc(BUF_SIZE+file_size);
+	*buffer=(char *)calloc(BUF_SIZE+file_size,1);
 
-	sprintf(*buffer, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %u\r\nConnection: close\r\n\r\n", content_types[get_mime(file_path)], file_size );
+	sprintf(*buffer, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent-Length: %u\r\nConnection: close\r\n\r\n", content_types[get_mime(file_path)], file_size );
 	file_size+=strlen(*buffer);
 	fread((*buffer+strlen(*buffer)), file_size, 1, file);
 	fclose(file);
@@ -65,6 +63,6 @@ int get_mime(char *file_path){
  *  \returns The size of the answer to send */
 int send_string(char **buffer, char *string){
 	*buffer=malloc(strlen(string)+BUF_SIZE);	
-	sprintf(*buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %u\r\nConnection: close\r\n\r\n%s", (unsigned int)strlen(string), string);
+	sprintf(*buffer, "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %u\r\nConnection: close\r\n\r\n%s", (unsigned int)strlen(string), string);
 	return strlen(*buffer);
 }
