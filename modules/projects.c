3d9ce2e296313de7308839eeeb36ba608e1ef8bb
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2013-10-06 12:24:19 +0200

    Server crashing with SIGSEGV on page load

--- modules/projects.c
+++ modules/projects.c
@@ -6,6 +6,12 @@ void *projects_close_module(void *arg)
     return NULL;
 }
 
+int print_projects(void *data, int num, char **arguments, char **colnames)
+{
+	//~ html_add_tag(&((struct html_ui*)client_request->answer)->sidebar, "<b>%s</b><br>", arguments[3]);
+	printf("%s\n", arguments[3]);
+}
+
 int projects_init_module(int id)
 {
 	printf("PROJECT MODULE INITIALIZED AT: %d\n", id);
@@ -27,6 +33,31 @@ int projects_request(void *module_data, request *client_request)
 	html=html_add_tag(&((struct html_ui*)client_request->answer)->sidebar, "<table style='margin-left:10px'", NULL,"</table>");
 	
 	//Project list
+	const char* data = "Callback function called";
+	char *zErrMsg = NULL;
+	
+	int db_handle, db_status;
+	
+	/** \todo Read database file from configuration */
+	if(sqlite3_open("lunkwill.db", &db_handle))
+	{
+		fprintf(stderr, "Could not open DB\n");
+		return 1;
+	}
+
+	db_status = sqlite3_exec(&db_handle, "SELECT * FROM tickets WHERE parent = 0", print_projects, (void*)data, &zErrMsg);
+	if(db_status != SQLITE_OK)
+	{
+		fprintf(stderr, "[FUUU]:\tdb_status = %d\n", db_status);
+		
+		if(zErrMsg == NULL)
+			fprintf(stderr, "[FUCK]:\t");
+		
+		fprintf(stderr, "sqlite error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+
+	sqlite3_close(db_handle);
 
 	return 0;
 
--- modules/projects.h
+++ modules/projects.h
@@ -9,8 +9,14 @@ struct projects_data
 	int i;
 };
 
+/** \brief Defined in lunkwill.h */
+extern sqlite3 *db_handle;
+
 /** \brief Exits module */
 void *projects_close_module(void *arg);
+
+/** \brief Callback function for sqlite3_exec. Prints the projects */
+int print_projects(void *data, int num, char **arguments, char **colnames);
 
 /** \brief Initalizes module */
 int projects_init_module(int id);
