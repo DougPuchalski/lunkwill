01d88eb5a529e636396bdf7d785e2f0a256fcd64
pentix <pentix@vm-0.pentix.koding.kd.io>
2013-11-23 16:07:48 +0000

    Edited dummy lua

--- 04-plugins/dummy/dummy.lua
+++ 04-plugins/dummy/dummy.lua
@@ -2,5 +2,5 @@ ModuleName="LuaDummy"
 ModuleDescription="Dummy LUA Module"
 
 function ModuleAnswerRequest(user, group, session1, session2, project, module, module_request)
-        return 0, "Test", module_request
+        return 0, "Sidebar", "LUA ARGS: USER:"..user.." GROUP:"..group.." SESSION1:"..session1.." SESSION2:"..session2.." PROJECT:"..project.." MODULE:"..module.." MODULE_REQUEST:"..module_request
 end
